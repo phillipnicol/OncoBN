5f644cadce10b9eb3ee17146dd8900eb67215f03
phillipnicol <43178367+phillipnicol@users.noreply.github.com>
2020-12-28 10:34:00 -0500

    Updated tests

--- r_package/R/plotCPN.R
+++ r_package/R/plotCPN.R
@@ -3,7 +3,7 @@ plotCPN <- function(fit) {
   g <- make_directed_graph(fit$edgelist)
   V(g)$color <- "white"
   V(g)$shape <- "none"
-  plot(g, label.color = "blue", label.cex = 1.6)
+  plot(g, label.color = "blue", label.cex = 1.6, edge.arrow.size = 0.5)
 }
 
 plotBootstrapCPN <- function(bs_out, threshold = 0.5) {
@@ -13,5 +13,5 @@ plotBootstrapCPN <- function(bs_out, threshold = 0.5) {
   V(g)$shape <- "none"
   E(g)$weight <- bs_out$counts[bs_out$counts/bs_out$N > threshold]/bs_out$N
   E(g)$color <- rgb(1-E(g)$weight,1-E(g)$weight,1-E(g)$weight)
-  plot(g, label.color = "blue", label.cex = 1.6, edge.color = E(g)$color)
+  plot(g, label.color = "blue", label.cex = 1.6, edge.color = E(g)$color, edge.arrow.size = 0.5)
 }
